bf16bb12cc66bdcb5dd41b8a849e58191deed627
Kyle O'Brien <obrienk@webbhost.net>
2020-03-07 17:12:43 -0600

    Add production and dev independent config files, corrected version to 0.0.2 (#1)
    * Add production and dev independent config files, corrected version number to 0.0.2
    
    * Add clean-webpack-plugin@3.0.0

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 const title = 'BigCom.dev';
 ReactDOM.render(<App title={title} />, document.getElementById('root'));
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 if (typeof window !== 'undefined') {
   React.render(<MainWrapper />, document.getElementById('root'));
 }
